34fd456a570b81b0d7fa75013b02d5ea489fee86
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-06-08 22:20:26 -0500

    Changes Image's path

--- src/lib/data/tech.ts
+++ src/lib/data/tech.ts
@@ -3,73 +3,73 @@ export const technologies = {
         {
             "id": "1",
             "name": "Javascript",
-            "img": "src/lib/data/img/JavaScript-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/JavaScript-logo.png?alt=media&token=fab9e487-5c5b-4edb-a5f4-e103b075d2a5",
         },
         {
             "id": "2",
             "name": "Typescript",
-            "img": "src/lib/data/img/typescript_logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/typescript_logo.png?alt=media&token=a112d9c7-eaac-4a8e-8dc0-ce7568a20547",
         },
         {
             "id": "3",
             "name": "Python",
-            "img": "src/lib/data/img/python_logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/python_logo.png?alt=media&token=1e79a2a5-ffac-4ef9-a888-35829193e7c5",
         },
         {
             "id": "4",
             "name": "C#",
-            "img": "src/lib/data/img/Csharp_Logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/Csharp_Logo.png?alt=media&token=a5492bf2-2b1c-40ce-a1f9-f078a88b5429",
         },
         {
             "id": "5",
             "name": "Java",  
-            "img": "src/lib/data/img/java-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/java-logo.png?alt=media&token=c75bc68e-27ac-4e02-b510-e3c3650c4fad",
         },
         {
             "id": "6",
             "name": "Dart",
-            "img": "src/lib/data/img/dart_logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/dart_logo.png?alt=media&token=04c4b1be-841a-4afb-9ef5-301fc8858bac",
         }
     ],
     "frameworksWeb": [
         {
             "id": "1",
             "name": "Angular",
-            "img": "src/lib/data/img/Angular-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/Angular-logo.png?alt=media&token=b474116e-267b-42cc-8bc9-c6191ac8bff0",
         },
         {
             "id": "2",
             "name": "React",
-            "img": "src/lib/data/img/React-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/React-logo.png?alt=media&token=50f668db-c88d-41f6-961f-5f651af7431a",
         },
         {
             "id": "3",
             "name": "Vue",
-            "img": "src/lib/data/img/Vue-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/Vue-logo.png?alt=media&token=ac36fd7e-0847-4c45-911b-66dda7e4c909",
         },
     ],
     "frameworksMobile": [
         {
             "id": "1",
             "name": "Ionic",
-            "img": "src/lib/data/img/Ionic-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/Ionic-logo.png?alt=media&token=a0b107e8-4e69-4f2b-a91a-e06bef015eee",
         },
         {
             "id": "2",
             "name": "React Native",
-            "img": "src/lib/data/img/React-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/React-logo.png?alt=media&token=50f668db-c88d-41f6-961f-5f651af7431a",
         },
         {
             "id": "3",
             "name": "Flutter",
-            "img": "src/lib/data/img/Flutter-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/Flutter-logo.png?alt=media&token=69eb3ed6-516d-487b-9af6-bc8306937fb4",
         },
     ],
     "backend": [
         {
             "id": "1",
             "name": "Express",
-            "img": "src/lib/data/img/Express-logo.png",
+            "img": "https://firebasestorage.googleapis.com/v0/b/flutter-varios-d288a.appspot.com/o/Express-logo.png?alt=media&token=90f29872-3b65-4e7b-9d7d-a5d8ab565096",
         }
     ]
 }
